aa28921fac1b0d877baf5789976846bf7b835cae
[firstname lastname] <mgrmanu10@gmail.com>
2023-03-31 13:17:30 +0200

    Usa Preferencias del Usuario

--- src/app/components/shared/theme-toggle/theme-toggle.component.ts
+++ src/app/components/shared/theme-toggle/theme-toggle.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-theme-toggle',
   templateUrl: './theme-toggle.component.html',
@@ -12,22 +13,39 @@ export class ThemeToggleComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    this.setTheme();
+    const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+    const userPrefersLight = window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches;
+  
+    if (userPrefersDark) {
+      localStorage.setItem('theme-color','dark');
+      this.storedTheme = localStorage.getItem('theme-color')!;
+      document.body.classList.add('dark-mode'); 
+      
+    }
+    if (userPrefersLight){
+      localStorage.setItem('theme-color','light');
+      this.storedTheme = localStorage.getItem('theme-color')!;
+      document.body.classList.remove('dark-mode')
+      
+    }
   }
+    
     //Setear tema oscuro o claro
     setTheme(){
-      if(this.storedTheme === 'theme-light'){
-        //toggle and update local storage to dark
-        localStorage.setItem('theme-color','theme-dark');
-        this.storedTheme = localStorage.getItem('theme-color')!;
-        document.body.classList.add('dark-mode')
-        
-      }else{
-        //toggle and update local storage to light
-        localStorage.setItem('theme-color','theme-light');
-        this.storedTheme = localStorage.getItem('theme-color')!;
-        document.body.classList.remove('dark-mode')
-      }
-    }
+    
+        if(this.storedTheme === 'light'){
+          //toggle and update local storage to dark
+          localStorage.setItem('theme-color','dark');
+          this.storedTheme = localStorage.getItem('theme-color')!;
+          document.body.classList.add('dark-mode')
+          
+        }else{
+          //toggle and update local storage to light
+          localStorage.setItem('theme-color','light');
+          this.storedTheme = localStorage.getItem('theme-color')!;
+          document.body.classList.remove('dark-mode')
+        }
 
+      }
 }
+
