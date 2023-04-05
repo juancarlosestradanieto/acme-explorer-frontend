097a3b19f7694822147ff35f795665e48bd37663
Juan Carlos Estrada Nieto <juancarlosestradanieto@gmail.com>
2023-04-05 13:29:45 +0200

    progress in trip add form

--- src/app/components/pages/trips/add-trip/add-trip.component.ts
+++ src/app/components/pages/trips/add-trip/add-trip.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-add-trip',
@@ -11,13 +11,19 @@ export class AddTripComponent implements OnInit {
   tripForm = this.fb.group({
     title: ['', Validators.required],
     description: ['', Validators.required],
-    price: ['', Validators.required],
-    requirements: ['', Validators.required],
+    price: ['', [Validators.required, Validators.min(1), Validators.max(999999)]],
+    //requirements: ['', Validators.required],
+    requirements: this.fb.array([
+      // this.getRequirement()
+    ]),
     startDate: ['', Validators.required],
     endDate: ['', Validators.required],
     publicationDate: ['', Validators.required],
     pictures: ['', Validators.required],
-    stages: ['', Validators.required],
+    //stages: ['', Validators.required],
+    stages: this.fb.array([
+      // this.getStage()
+    ]),
     managerId: ['', Validators.required],
   });
   error_message!: string;
@@ -29,6 +35,42 @@ export class AddTripComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+
+  private getRequirement()
+  {
+    return this.fb.group({
+      requirement: ['', Validators.required],
+    });
+  }
+
+  onAddRequirement() {
+    const control = <FormArray>this.tripForm.controls['requirements'];
+    control.push(this.getRequirement());
+  }
+
+  onRemoveRequirement(i: number) {
+    const control = <FormArray>this.tripForm.controls['requirements'];
+    control.removeAt(i);
+  }
+
+  private getStage()
+  {
+    return this.fb.group({
+      title: ['', Validators.required],
+      description: ['', Validators.required],
+      price: ['', [Validators.required, Validators.min(1), Validators.max(999999)]],
+    });
+  }
+
+  onAddStage() {
+    const control = <FormArray>this.tripForm.controls['stages'];
+    control.push(this.getStage());
+  }
+
+  onRemoveStage(i: number) {
+    const control = <FormArray>this.tripForm.controls['stages'];
+    control.removeAt(i);
   }
 
   onSubmit()
