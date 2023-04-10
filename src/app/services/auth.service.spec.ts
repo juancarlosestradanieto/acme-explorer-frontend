54c80fe44f0b852992631598200eeb1829dccba4
Angel Caballero Dominguez <56036551+angcabdom@users.noreply.github.com>
2023-04-10 19:09:59 +0200

    Fix register test

--- src/app/services/auth.service.spec.ts
+++ src/app/services/auth.service.spec.ts
@@ -8,17 +8,18 @@ import { environment } from 'src/environments/environment';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Actor } from '../models/actor.model';
+import { FormBuilder } from '@angular/forms';
 
 describe('AuthService', () => {
   let service: AuthService;
-
+  let fb: FormBuilder;
 
   let newActor: Actor = Actor.castJsonActor({
-    "phone": "123456789",
-    "name": "Prueba explorer 3",
-    "surname": "Prueba",
+    "phone_number": "123456789",
+    "name": "test",
+    "surname": "test",
     "email": randomEmailGenerator(12),
-    "password": "explorer123456",
+    "password": "1234567890",
     "address": "Calle prueba",
     "isActive": true,
     "role": ["EXPLORER"]
@@ -38,7 +39,7 @@ describe('AuthService', () => {
     TestBed.configureTestingModule({
       imports: [HttpClientModule,
         AngularFireModule.initializeApp(environment.firebase)],
-      providers: [AuthService, AngularFirestore, AngularFireAuth]
+      providers: [AuthService, AngularFirestore, AngularFireAuth, FormBuilder]
     });
 
     service = TestBed.get(AuthService);
@@ -51,11 +52,30 @@ describe('AuthService', () => {
   });
 
   it('should register an user', async () => {
-    let created = await service.registerUser(newActor);
+    fb = TestBed.inject(FormBuilder);
+    let newActorRF = fb.group({
+      name: newActor.getName(),
+      surname: newActor.getSurname(),
+      email: newActor.getEmail(),
+      password: newActor.getPassword(),
+      phone: newActor.getPhone_number(),
+      address: newActor.getAddress(),
+      role: newActor.getRole(),
+      isActive: newActor.isIsActive()
+    });
+
+    let created = await service.registerUser(newActorRF.value);
     console.log("Registered user ->", created)
 
     expect(created).toBeDefined();
-    expect(created).toEqual(jasmine.objectContaining({ _email: newActor.getEmail(), _password: newActor.getPassword() }));
+    expect(created).toEqual(jasmine.objectContaining({ 
+      name: newActor.getName(),
+      surname: newActor.getSurname(),
+      email: newActor.getEmail(),
+      phone: newActor.getPhone_number(),
+      address: newActor.getAddress(),
+      role: newActor.getRole(),
+      isActive: newActor.isIsActive() }));
   });
 
   it('should login an user', async () => {
@@ -76,7 +96,7 @@ describe('AuthService', () => {
     expect(service.isLoggedIn()).toBeFalsy();
   });
 
-})
+});
 
 function randomEmailGenerator(length: number): string {
   var randomChars = 'QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890';
