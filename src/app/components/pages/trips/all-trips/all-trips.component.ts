c060accae9845da4f4a0559ca75c529a06a21481
Angel Caballero Dominguez <56036551+angcabdom@users.noreply.github.com>
2023-04-12 21:11:54 +0200

    Add favouriting option on display and list

--- src/app/components/pages/trips/all-trips/all-trips.component.ts
+++ src/app/components/pages/trips/all-trips/all-trips.component.ts
@@ -14,7 +14,7 @@ import { TripsService } from 'src/app/services/trips/trips.service';
 })
 export class AllTripsComponent implements OnInit {
 
-  storeTheme:string = localStorage.getItem('theme-color')!;
+  storeTheme: string = localStorage.getItem('theme-color')!;
   trips: Array<Trip> = [];
   protected user!: Actor | null;
   protected activeRole: string = 'anonymous';
@@ -23,40 +23,37 @@ export class AllTripsComponent implements OnInit {
   total_pages: number = 0;
   pages: Array<number> = [];
 
-  constructor(private tripsService: TripsService, private authService: AuthService)
-  {
+  constructor(private tripsService: TripsService, private authService: AuthService) {
     this.currentDateTime = new Date;
   }
 
-  getTrips(page: number)
-  {
+  getTrips(page: number) {
     this.tripsService.getAllTrips(page)
       .then((response: any) => {
 
-      console.log("AllTripsComponent->constructor tripsService.getAllTrips then response ", response);
+        console.log("AllTripsComponent->constructor tripsService.getAllTrips then response ", response);
 
-      this.total_pages = response.totalPages;
+        this.total_pages = response.totalPages;
 
-      this.pages = [];
-      for (let page = 1; page <= this.total_pages; page ++)
-      {
-        this.pages.push(page);
-      }
+        this.pages = [];
+        for (let page = 1; page <= this.total_pages; page++) {
+          this.pages.push(page);
+        }
 
-      let json_trips = response.docs;
-      //console.log("json_trips ", json_trips);
-      let casted_trips = Trip.castJsonTrips(json_trips);
-      //console.log("casted_trips ", casted_trips);
-      
-      this.trips = casted_trips;
-      
-      //console.log(this.getDiffDays(this.trips[0].start_date.toString(), this.currentDateTime.toISOString()))
-    })
-    .catch((error: any) => {
+        let json_trips = response.docs;
+        //console.log("json_trips ", json_trips);
+        let casted_trips = Trip.castJsonTrips(json_trips);
+        //console.log("casted_trips ", casted_trips);
+
+        this.trips = casted_trips;
+
+        //console.log(this.getDiffDays(this.trips[0].start_date.toString(), this.currentDateTime.toISOString()))
+      })
+      .catch((error: any) => {
 
         console.error("AllTripsComponent->constructor tripsService.getAllTrips catch ", error);
 
-    });
+      });
   }
 
   ngOnInit(): void {
@@ -64,27 +61,24 @@ export class AllTripsComponent implements OnInit {
     this.getTrips(1);
 
     this.user = this.authService.getCurrentActor();
-    if(this.user) 
-    {
+    if (this.user) {
       this.activeRole = this.user.getRole().toString();
-    } 
-    else 
-    {
+    }
+    else {
       this.activeRole = 'anonymous';
     }
 
   }
-  
+
   getDiffDays(start: string, now: string) {
     var startDate = new Date(start);
     var endDate = new Date(now);
-  
+
     var difference = startDate.getTime() - endDate.getTime();
     return difference / (1000 * 3600 * 24);
   }
 
-  getCurrentStyles(start: string, now: string) 
-  {
+  getCurrentStyles(start: string, now: string) {
     let difference = this.getDiffDays(start, now);
     //console.log(difference);
     let soon = difference > 0 && difference <= 7;
@@ -98,10 +92,57 @@ export class AllTripsComponent implements OnInit {
     return currenStyles;
   }
 
-  deleteTrip(trip: Trip)
-  {
+  deleteTrip(trip: Trip) {
     console.log("deleteTrip", trip.id);
-    
+
+  }
+
+  favouriteTrip(trip: Trip) {
+    console.log("favouriteTrip user.email ", this.user!.getEmail());
+    console.log("favouriteTrip user.email-favourites ", this.user!.getEmail() + "-favourites");
+    let favourites = localStorage.getItem(this.user!.getEmail() + "-favourites");
+    console.log("favouriteTrip " + this.user!.getEmail() + "-favourites ", favourites);
+    if (!favourites) {
+      let newFavourites = [];
+      newFavourites.push(trip);
+      console.log("favouriteTrip newFavourites ", newFavourites);
+      localStorage.setItem(this.user!.getEmail() + "-favourites", JSON.stringify(newFavourites));
+    }
+    else {
+      let storedFavourites: Trip[] = JSON.parse(localStorage.getItem(this.user!.getEmail() + "-favourites") || '{}');
+      let existsInFavourites: boolean = storedFavourites.some(storedTrip => Trip.castJsonTrip(storedTrip).getTicker() === trip.getTicker());
+      console.log("favouriteTrip trip is in storedFavourites ", existsInFavourites);
+      if (existsInFavourites) {
+        storedFavourites.forEach((storedTrip, index) => {
+          if (Trip.castJsonTrip(storedTrip).getTicker() === trip.getTicker()) {
+            storedFavourites.splice(index, 1);
+          }
+        });
+        console.log("favouriteTrip storedFavourites ", storedFavourites);
+        localStorage.setItem(this.user!.getEmail() + "-favourites", JSON.stringify(storedFavourites));
+      } else {
+        console.log("favouriteTrip storedFavourites ", storedFavourites);
+        storedFavourites.push(trip);
+        localStorage.setItem(this.user!.getEmail() + "-favourites", JSON.stringify(storedFavourites));
+      }
+    }
+  }
+
+  checkTripInFavourites(trip: Trip) {
+    let result: boolean = false;
+    console.log("favouriteTrip user.email ", this.user!.getEmail());
+    console.log("favouriteTrip user.email-favourites ", this.user!.getEmail() + "-favourites");
+    let favourites = localStorage.getItem(this.user!.getEmail() + "-favourites");
+    console.log("favouriteTrip " + this.user!.getEmail() + "-favourites ", favourites);
+    if (favourites) {
+      let storedFavourites: Trip[] = JSON.parse(localStorage.getItem(this.user!.getEmail() + "-favourites") || '{}');
+      let existsInFavourites: boolean = storedFavourites.some(storedTrip => Trip.castJsonTrip(storedTrip).getTicker() === trip.getTicker());
+      console.log("favouriteTrip trip is in storedFavourites ", existsInFavourites);
+      if (existsInFavourites) {
+        result = true;
+      }
+    }
+    return result;
   }
 
 }
