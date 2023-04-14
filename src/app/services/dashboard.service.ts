703efde41a2152eda57661c9ecc17d509c4819b1
[firstname lastname] <mgrmanu10@gmail.com>
2023-04-14 18:50:15 +0200

    Dashboard: Avg Prices Finder and Top10KeyWords OK

--- src/app/services/dashboard.service.ts
+++ src/app/services/dashboard.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 const httpOptions = {
@@ -45,4 +46,23 @@ export class DashboardService {
 
     });
   }
+
+  //*The average price range that explorers indicate in their finders.
+  //*The top 10 key words that the explorers indicate in their finders.
+  //*Al no disponer de un job en el backend que nos devuelvan dichos datos, se han tomado
+  //*dos endpoints de APIs alojadas en la nube que devuelven palabras y números random
+
+  //?The average price range that explorers indicate in their finders.
+  getTop10KeyWords():Observable<any>{
+    const url="https://random-word-api.vercel.app/api?words=10";
+    return this.http.get<any>(url);
+  }
+
+   //?The average price range that explorers indicate in their finders.
+
+    getAveragePriceFinders():Observable<any>{
+      const url="https://random-data-api.com/api/number/random_number"
+      return this.http.get<any>(url);
+    }
+
 }
