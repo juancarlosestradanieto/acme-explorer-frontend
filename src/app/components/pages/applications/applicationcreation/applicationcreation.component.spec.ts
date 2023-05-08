c9368ea08037dda8bfb4a5317d653bc43449b146
Angel Caballero Dominguez <56036551+angcabdom@users.noreply.github.com>
2023-05-08 14:11:52 +0200

    Add check if trip started functionality and test for application creation

--- src/app/components/pages/applications/applicationcreation/applicationcreation.component.spec.ts
+++ src/app/components/pages/applications/applicationcreation/applicationcreation.component.spec.ts
@@ -10,13 +10,20 @@ import { environment } from 'src/environments/environment';
 import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
 import { Actor } from 'src/app/models/actor.model';
+import { Trip } from 'src/app/models/trip.model';
+import { Application } from 'src/app/models/application.model';
+import { ActivatedRoute } from '@angular/router';
+import { TripsService } from 'src/app/services/trips/trips.service';
+import { ApplicationsService } from 'src/app/services/applications.service';
 
 export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
   return new TranslateHttpLoader(http);
 }
 
+let explorer_id = '64304c9dae7efef4708a796f';
+
 const mockActor = Actor.castJsonActor({
-  "_id": "64304c9dae7efef4708a796f",
+  "_id": explorer_id,
   "phone": "test",
   "name": "test",
   "surname": "test",
@@ -32,9 +39,64 @@ const mockActor = Actor.castJsonActor({
   "__v": 0,
 });
 
+
+let trip_id = '6434467d297d879c26d2336d';
+
+let mockTrip = Trip.castJsonTrip({
+  "canceled": false,
+  "cancelReason": "",
+  "description": "Sed sequi accusantium sed consequuntur voluptas accusantium eaque expedita.",
+  "endDate": "2030-11-02",
+  "id": "6434467d297d879c26d2336d",
+  "managerId": "6434467d297d879c26d23348",
+  "pictures": [],
+  "price": 105,
+  "publicationDate": "2020-08-20",
+  "requirements": [],
+  "stages": [],
+  "startDate": "2030-10-31",
+  "ticker": "7O{Q@v6)1)*",
+  "title": "A trip to CHIPIONA",
+  "_id": "6434467d297d879c26d2336d",
+});
+
+let mockStartedTrip = Trip.castJsonTrip({
+  "canceled": false,
+  "cancelReason": "",
+  "description": "Sed sequi accusantium sed consequuntur voluptas accusantium eaque expedita.",
+  "endDate": "2010-11-02",
+  "id": "6434467d297d879c26d2336d",
+  "managerId": "6434467d297d879c26d23348",
+  "pictures": [],
+  "price": 105,
+  "publicationDate": "2020-08-20",
+  "requirements": [],
+  "stages": [],
+  "startDate": "2010-10-31",
+  "ticker": "7O{Q@v6)1)*",
+  "title": "A trip to CHIPIONA",
+  "_id": "6434467d297d879c26d2336d",
+});
+
+let tripApplication = new Application({
+  "applicationMoment": "2023-04-09T04:22:38.500Z",
+  "comments": "Quam eius voluptas",
+  "status": "PENDING",
+  "explorer_Id": explorer_id,
+  "trip_Id": trip_id,
+  "rejected_reason": null
+});
+
 describe('ApplicationcreationComponent', () => {
   let component: ApplicationcreationComponent;
   let fixture: ComponentFixture<ApplicationcreationComponent>;
+
+  let createApplicationsSpy: any;
+  let getApplicationsByExplorerIdSpy: any;
+  let getSingleTripSpy: any;
+
+  let tripSpy = jasmine.createSpyObj('TripService', ['getSingleTrip']);
+  let applicationSpy = jasmine.createSpyObj('ApplicationsService', ['createApplication', 'getApplicationsByExplorerId']);
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -50,13 +112,27 @@ describe('ApplicationcreationComponent', () => {
             deps: [HttpClient]
           }
         })
+      ], providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: {
+            snapshot: { params: { id: trip_id } }
+          }
+        },
+        {
+          provide: TripsService,
+          useValue: tripSpy
+        },
+        {
+          provide: ApplicationsService,
+          useValue: applicationSpy
+        }
       ],
       declarations: [ApplicationcreationComponent]
     })
       .compileComponents();
 
     let authService = TestBed.inject(AuthService);
-
     spyOn(authService, 'getCurrentActor').and.returnValue(mockActor);
 
     fixture = TestBed.createComponent(ApplicationcreationComponent);
@@ -64,7 +140,44 @@ describe('ApplicationcreationComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  afterEach(() => {
+    getSingleTripSpy.calls.reset();
+    createApplicationsSpy.calls.reset();
+    getApplicationsByExplorerIdSpy.calls.reset();
+  })
+
+  it('should apply for not started trip', async () => {
+    getSingleTripSpy = tripSpy.getSingleTrip.and.returnValue(Promise.resolve(mockTrip));
+    createApplicationsSpy = applicationSpy.createApplication.and.returnValue(Promise.resolve(tripApplication));
+    getApplicationsByExplorerIdSpy = applicationSpy.getApplicationsByExplorerId.and.returnValue(Promise.resolve([]));
+
+    await component.getTrip();
+
+    await component.onApplicationSubmit();
+
     expect(component).toBeTruthy();
+    expect(getSingleTripSpy).toHaveBeenCalledWith(trip_id);
+    expect(createApplicationsSpy).toHaveBeenCalledWith(component.applicationForm.value);
+    expect(getApplicationsByExplorerIdSpy).toHaveBeenCalledWith(explorer_id);
+    expect(component.trip_started).toBeFalsy();
+    expect(component.application_exists).toBeFalsy();
+  });
+
+  it('should not apply for already started trip', async () => {
+    getSingleTripSpy = tripSpy.getSingleTrip.and.returnValue(Promise.resolve(mockStartedTrip));
+    createApplicationsSpy = applicationSpy.createApplication.and.returnValue(Promise.resolve(tripApplication));
+    getApplicationsByExplorerIdSpy = applicationSpy.getApplicationsByExplorerId.and.returnValue(Promise.resolve([]));
+
+    await component.getTrip();
+
+    await component.onApplicationSubmit();
+
+    expect(component).toBeTruthy();
+    expect(getSingleTripSpy).toHaveBeenCalledWith(trip_id);
+    expect(createApplicationsSpy).not.toHaveBeenCalledWith(component.applicationForm.value);
+    expect(getApplicationsByExplorerIdSpy).not.toHaveBeenCalledWith(explorer_id);
+    expect(component.trip_started).toBeTruthy();
+    expect(component.application_created).toBeFalsy();
+    expect(component.application_exists).toBeFalsy();
   });
 });
--- src/app/components/pages/applications/applicationcreation/applicationcreation.component.ts
+++ src/app/components/pages/applications/applicationcreation/applicationcreation.component.ts
@@ -27,20 +27,22 @@ export class ApplicationcreationComponent implements OnInit {
 
   application_created: boolean = false;
 
+  trip_started: boolean = false;
+
   constructor(private tripService: TripsService, private applicationService: ApplicationsService, private authService: AuthService,
     private fb: FormBuilder, private route: ActivatedRoute,
     private router: Router) {
-    this.trip_id = this.route.snapshot.params['id'];
-    this.applicationForm = this.fb.group({
-      status: ['PENDING'],
-      trip_Id: [''],
-      explorer_Id: [''],
-      tripPrice: [''],
-      comments: this.fb.array([
-        this.fb.control('')
-      ])
-    });
-    this.getTrip();
+      this.trip_id = this.route.snapshot.params['id'];
+      this.applicationForm = this.fb.group({
+        status: ['PENDING'],
+        trip_Id: [''],
+        explorer_Id: [''],
+        tripPrice: [''],
+        comments: this.fb.array([
+          this.fb.control('')
+        ])
+      });
+      this.getTrip();
   }
 
   createForm() {
@@ -72,37 +74,39 @@ export class ApplicationcreationComponent implements OnInit {
   }
 
   onApplicationSubmit() {
-
+    if (this.getNowAndStartDateDiffInDays(this.trip) <= 0) {
+      this.trip_started = true;
+      console.error("ApplicationcreationComponent->onApplicationSubmit error ", 'trip already started');
+    }
+    else {
     this.applicationService.getApplicationsByExplorerId(this.user!.id)
       .then((response) => {
         console.log("ApplicationcreationComponent->onApplicationSubmit getApplicationsByExplorerId then response ", response);
         let casted_application = Application.castJsonApplications(response);
         this.application_exists = casted_application.filter((application: Application) => application.trip_Id == this.trip_id).length > 0;
 
-        console.log("ApplicationcreationComponent->onApplicationSubmit getApplicationsByExplorerId then application_exists ", this.application_exists);
-      
-      if (!this.application_exists) {
-        this.applicationService.createApplication(this.applicationForm.value)
-        .then((response) => {
-  
-          console.log("ApplicationcreationComponent->onApplicationSubmit then response ", response);
-          this.applicationForm.reset();
+        if (!this.application_exists) {
+          this.applicationService.createApplication(this.applicationForm.value)
+            .then((response) => {
 
-          this.application_created = true;
-  
-        })
-        .catch((error) => {
-  
-          console.error("ApplicationcreationComponent->onApplicationSubmit error ", error);
-  
-        });
-      }
-      
-      
+              console.log("ApplicationcreationComponent->onApplicationSubmit applicationService.createApplication then response ", response);
+              this.applicationForm.reset();
+
+              this.application_created = true;
+            })
+            .catch((error) => {
+
+              console.error("ApplicationcreationComponent->onApplicationSubmit applicationService.createApplication error ", error);
+
+            });
+        }
+
+
       })
       .catch((error) => {
-        console.log("ApplicationcreationComponent->onApplicationSubmit getApplicationsByExplorerId catch ", error);
+        console.log("ApplicationcreationComponent->onApplicationSubmit applicationService.getApplicationsByExplorerId getApplicationsByExplorerId catch ", error);
       });
+    }
   }
 
   async getTrip() {
@@ -115,6 +119,7 @@ export class ApplicationcreationComponent implements OnInit {
 
         let casted_trip: Trip = Trip.castJsonTrip(this.json_trip);
         this.trip = casted_trip;
+        console.log("ApplicationcreationComponent->constructor tripsService.getSingleTrip this.trip ", this.trip);
         this.applicationForm = this.createForm();
       })
       .catch((error) => {
@@ -124,6 +129,20 @@ export class ApplicationcreationComponent implements OnInit {
   }
 
   ngOnInit(): void {
+  }
+
+  getDiffDays(start: string, now: string) {
+    var startDate = new Date(start);
+    var endDate = new Date(now);
+
+    var difference = startDate.getTime() - endDate.getTime();
+    return difference / (1000 * 3600 * 24);
+  }
+
+  getNowAndStartDateDiffInDays(trip: Trip) {
+    let currentDateTime = new Date;
+    console.log("ApplicationcreationComponent->getNowAndStartDateDiffInDays trip.getStartDate() ", trip.getStartDate());
+    return this.getDiffDays(trip.getStartDate().toString(), currentDateTime.toISOString());
   }
 
 }
