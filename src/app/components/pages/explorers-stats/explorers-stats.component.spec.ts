bed3e9d99778772d255d495b09da3e385608bc17
Juan Carlos Estrada Nieto <juancarlosestradanieto@gmail.com>
2023-05-12 12:10:31 +0200

    fixed tests

--- src/app/components/pages/explorers-stats/explorers-stats.component.spec.ts
+++ src/app/components/pages/explorers-stats/explorers-stats.component.spec.ts
@@ -1,6 +1,14 @@
+import { HttpClient, HttpHandler } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder } from '@angular/forms';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 
 import { ExplorersStatsComponent } from './explorers-stats.component';
+
+export function HttpLoaderFactory(http: HttpClient): TranslateHttpLoader {
+  return new TranslateHttpLoader(http);
+}
 
 describe('ExplorerStatsComponent', () => {
   let component: ExplorersStatsComponent;
@@ -8,7 +16,21 @@ describe('ExplorerStatsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ExplorersStatsComponent ]
+      declarations: [ ExplorersStatsComponent ],
+      imports: [
+        TranslateModule.forRoot({
+          loader: {
+            provide: TranslateLoader,
+            useFactory: HttpLoaderFactory,
+            deps: [HttpClient]
+          }
+        })
+      ],
+      providers: [
+        HttpClient,
+        HttpHandler,
+        FormBuilder
+      ]
     })
     .compileComponents();
 
--- src/app/services/explorers-stats/explorers-stats.service.spec.ts
+++ src/app/services/explorers-stats/explorers-stats.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpHandler } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { ExplorersStatsService } from './explorers-stats.service';
@@ -6,7 +7,9 @@ describe('ExplorerStatsService', () => {
   let service: ExplorersStatsService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [HttpClient, HttpHandler]
+    });
     service = TestBed.inject(ExplorersStatsService);
   });
 
